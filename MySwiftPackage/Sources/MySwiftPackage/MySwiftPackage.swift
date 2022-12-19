e53df3b206020cf07d20e329e5b33616d83fb09f
KEN LEE <77721629+kenLEE2000@users.noreply.github.com>
2022-12-19 23:19:04 +0800

    Add files via upload

---
+++ MySwiftPackage/Sources/MySwiftPackage/MySwiftPackage.swift
@@ -0,0 +1,10 @@
+public struct MySwiftPackage {
+    public private(set) var text = "Hello, World!"
+    
+    public static func hello() {
+        print("Hello world")
+    }
+    
+    public init() {
+    }
+}
